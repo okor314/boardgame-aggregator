4356556eaf83d9d18979252de3bd2a1ddf5b8299
okor314 <korol.oleg10@gmail.com>
2025-09-22 00:01:03 +0300

    Add function for scraping game detail page

--- scripts/geekach_scraper.py
+++ scripts/geekach_scraper.py
@@ -1,12 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+from proxy import Proxy
 
 def errorCatcher(func, heandler, *args, **kwargs):
     try:
         return func(*args, **kwargs)
     except Exception as e:
         return heandler(e)
+
+def getGameData(url: str, proxy: Proxy, pause: float = 0):
+    page = requests.get(url, proxies=proxy.proxyForRequests(5))
+    if page.status_code != 200:
+        return None
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    title = soup.find('h1', attrs={'class': 'product-title'}).text.strip().replace('Настільна гра ', '')
+    in_stock = errorCatcher(lambda _: soup.find('div', attrs={'class': 'product-header__availability'}).text.strip(),
+                      lambda _: None, None)
+    price = errorCatcher(lambda _: float(soup.find('meta', attrs={'itemprop': 'price'}).get('content')),
+                         lambda _: None, None)
+    
+    # Extracting data from the features table
+    table = soup.find('table', attrs={'class': 'product-features__table'})
+    rows = table.find_all('tr')
+    players = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Гравців' in row.text][0],
+                           lambda _: None, None)
+    age = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Вік' in row.text][0],
+                       lambda _: None, None)
+    maker = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Видавець' in row.text][0],
+                         lambda _: None, None)
+    bbg_url = errorCatcher(lambda _: [row.find('td').find('a').get('href') for row in rows if 'BGG' in row.text][0],
+                           lambda _: None, None)
+    id = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Артикул' in row.text][0],
+                           lambda _: None, None)
+    
+    
+    # Structuring data to dict
+    data = {
+        'id': id,
+        'title': title,
+        'in_stock': in_stock,
+        'price': price,
+        'players': players,
+        'age': age,
+        'maker': maker,
+        'bbg_url': bbg_url
+    }
+
+    # Pause before making next request
+    time.sleep(pause)
+    
+    return data
 
 def scrapeGames(pageSoup, dataDictionary):
     gamePreviews = pageSoup.find_all('li', attrs={'class': 'catalog-grid__item'})
