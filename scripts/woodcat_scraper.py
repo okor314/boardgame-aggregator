f7505a1f7418333c61ef43635f8d38570899b51d
okor314 <korol.oleg10@gmail.com>
2025-09-22 14:23:00 +0300

    Add function to scrape game detail page

--- scripts/woodcat_scraper.py
+++ scripts/woodcat_scraper.py
@@ -1,6 +1,57 @@
-from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+from proxy import Proxy
+
+def errorCatcher(func, heandler, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        return heandler(e)
+    
+def getGameData(url: str, proxy: Proxy, pause: float = 0):
+    page = requests.get(url, proxies=proxy.proxyForRequests(5))
+    if page.status_code != 200:
+        return None
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    id = errorCatcher(lambda _: soup.find('div', attrs={'class': 'product-header__code product-header__code--filled'}).text.strip().replace('Артикул: ', ''),
+                      lambda _: None, None)
+    title = soup.find('h1', attrs={'class': 'product-title'}).text.strip().replace('Настільна гра ', '')
+    in_stock = errorCatcher(lambda _: soup.find('div', attrs={'class': 'product-header__availability'}).text.strip(),
+                      lambda _: None, None)
+    price = errorCatcher(lambda _: float(soup.find('meta', attrs={'itemprop': 'price'}).get('content')),
+                         lambda _: None, None)
+    
+    # Extracting data from the features table
+    table = soup.find('table', attrs={'class': 'product-features__table'})
+    rows = table.find_all('tr')
+    players = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Кількість гравців' in row.text][0],
+                           lambda _: None, None)
+    age = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Вік' in row.text][0],
+                       lambda _: None, None)
+    maker = errorCatcher(lambda _: [row.find('td').text.strip() for row in rows if 'Видавець' in row.text][0],
+                         lambda _: None, None)
+    
+    # Structuring data to dict
+    data = {
+        'id': id,
+        'title': title,
+        'in_stock': in_stock,
+        'price': price,
+        'players': players,
+        'age': age,
+        'maker': maker,
+        'bbg_url': bbg_url
+    }
+
+    # Pause before making next request
+    time.sleep(pause)
+    
+    return data
+
 
 def scrapeGames(pageSoup, dataDictionary):
     links = ['https://woodcat.com.ua' + game.find('a').get('href') for game in pageSoup.find_all('div', attrs={'class':'catalogCard-view'})]
