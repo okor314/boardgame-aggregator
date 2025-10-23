71aaaddb27f919fbe93df8ff426b3a654c0755c8
okor314 <korol.oleg10@gmail.com>
2025-10-23 19:46:17 +0300

    Add update function for history table

--- database/history_database.py
+++ database/history_database.py
@@ -7,7 +7,26 @@ def createHistoryTable(connection):
                    game_id INT,
                    site_id SMALLINT,
                    price REAL,
-                   checkdate DATE);""")
+                   checkdate DATE,
+                   PRIMARY KEY (game_id, site_id, checkdate));""")
+    connection.commit()
+    cursor.close()
+
+def updateHistoryTable(connection):
+    cursor = connection.cursor()
+    # Get table names with site data
+    cursor.execute("""SELECT id, name FROM site
+                   ORDER BY id;""")
+    sites = cursor.fetchall()
+
+    for site_id, tableName in sites:
+        cursor.execute(f"""INSERT INTO history (game_id, site_id, price, checkdate)
+                        SELECT game.id, {site_id}, t.price, t.lastchecked FROM game
+                        INNER JOIN {tableName} AS t
+                        ON game.{tableName}_id = t.id
+                        ORDER BY game.id
+                        ON CONFLICT(game_id, site_id, checkdate)
+                        DO NOTHING; """)
     connection.commit()
     cursor.close()
 
